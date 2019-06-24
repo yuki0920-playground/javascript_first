6e50e5ce40fd25fb3766128f712be9233054b8b0
yuki0920 <ywsep20@gmail.com>
2019-06-24 18:37:59 +0900

    四則演算

--- myscript.js
+++ myscript.js
@@ -1,7 +1,10 @@
-// nullとundefined
-
-var x = null;
-console.log(x);
-
-var y;
-console.log(y);
+// 足し算
+console.log(10 + 3);
+// 引き算
+console.log(10 - 3);
+// 掛け算
+console.log(10 * 3);
+// 割り算
+console.log(10 / 3);
+// 余り
+console.log(10 % 3);
