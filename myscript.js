dc40652541d9680a7f28acba9b36af2a591f4033
yuki0920 <ywsep20@gmail.com>
2019-06-24 18:12:21 +0900

    console.log

--- myscript.js
+++ myscript.js
@@ -1,1 +1,15 @@
-console.log("Hello World from myscript");
+// ブラウザのコンソールに出力.デバッグに役立つ
+console.log("メッセージを出力");
+
+// 変数を出力
+var i = 10;
+console.log(i);
+
+// 情報として出力
+console.info("情報です");
+
+// 警告として出力
+console.warn("警告です");
+
+// エラーとして出力
+console.error("エラーです");
