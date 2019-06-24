859158af35b31c4c5c2504ad86d2f3c076a10846
yuki0920 <ywsep20@gmail.com>
2019-06-24 18:22:29 +0900

    変数

--- myscript.js
+++ myscript.js
@@ -1,15 +1,3 @@
-// ブラウザのコンソールに出力.デバッグに役立つ
-console.log("メッセージを出力");
-
-// 変数を出力
-var i = 10;
-console.log(i);
-
-// 情報として出力
-console.info("情報です");
-
-// 警告として出力
-console.warn("警告です");
-
-// エラーとして出力
-console.error("エラーです");
+// 変数の出力
+var colorName = "red";
+// console.log(colorName);
