5d29d4c1d8c4fc9e769b5f74cb571f61f98f68fe
yuki0920 <ywsep20@gmail.com>
2019-06-24 20:00:37 +0900

    functionコンストラクター

--- myscript.js
+++ myscript.js
@@ -1,5 +1,3 @@
-var getRectangle = function(height, width) {
-  return height * width;
-}
+var getRectangle = new Function('height', 'width', 'return height * width');
 
 console.log(getRectangle(3, 5));
